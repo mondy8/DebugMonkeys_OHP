83f96adc7ab4e33bbfd0f46a4f86db73833f5466
ImaokaHiroaki <wmoaiw@gmail.com>
2024-05-03 14:23:22 +0900

    ヘッダーをheadlessUIで作成

--- debug_monkeys_ohp/src/components/Header.tsx
+++ debug_monkeys_ohp/src/components/Header.tsx
@@ -1,13 +1,11 @@
 "use client";
 
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Header() {
-  const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
-  const toggleMenu = () => setMenuOpen((prevState) => !prevState);
-
   const game = [
     "papito",
     "elaa",
@@ -22,10 +20,13 @@ export default function Header() {
   ];
 
   return (
-    <header className="border-gray border-b px-10 py-4">
-      <div className="flex flex-row justify-between align-middle">
+    <header className="border-gray relative border-b px-10 py-4">
+      <div className="align-middl e flex flex-row justify-between">
         <h1 className="transition-transform hover:-translate-y-1">
-          <Link href="/" className="">
+          <Link
+            href="/"
+            className="block rounded-md p-1 focus:outline-none focus-visible:ring"
+          >
             <Image
               src="/common/logo.svg"
               width={170}
@@ -34,27 +35,51 @@ export default function Header() {
             />
           </Link>
         </h1>
-        <button
-          aria-controls="menu"
-          aria-expanded={isMenuOpen}
-          onClick={toggleMenu}
-          className=""
-        >
-          MENU
-          <span className="sr-only">メニュー開閉</span>
-        </button>
+        <Popover>
+          {({ open }) => (
+            <>
+              <Popover.Button
+                className={`
+                ${open ? "text-gray-900" : "text-gray-950"}
+                group inline-flex items-center rounded-md bg-white px-3 py-2 text-base font-medium transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring`}
+              >
+                <span>MENU</span>
+                <span className="sr-only">メニュー開閉</span>
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute right-0 z-10 mt-3 w-screen max-w-sm px-4 md:right-10 md:px-0 lg:max-w-2xl">
+                  <nav className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
+                    <ul className="relative grid gap-2 bg-white p-7 lg:grid-cols-2">
+                      {game.map((game) => {
+                        return (
+                          <li key={game}>
+                            <Link
+                              href={`/detail/${game}/`}
+                              className="flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
+                            >
+                              <p className="text-sm font-medium text-gray-900">
+                                {game}
+                              </p>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </nav>
+                </Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
       </div>
-      <nav id="menu" hidden={!isMenuOpen}>
-        <ul>
-          {game.map((game) => {
-            return (
-              <li key={game}>
-                <Link href={`/detail/${game}/`}>{game}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
     </header>
   );
 }
